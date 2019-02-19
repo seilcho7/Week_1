08f6e2b2fe7d0f83ca205e023cb9bd25c2789add
Seil Cho <seilcho7@hotmail.com>
2019-02-19 18:21:04 -0500

    multiply user input

---
+++ prompt-for-number.py
@@ -0,0 +1,26 @@
+# prompt user for a number then multiply that number by itself
+
+##################################################################
+# 1. Setup and configuration (set up your variables)
+##################################################################
+
+# promt user for a number
+user_input = int(input("Please enter a number: "))
+# the input() function call is replaced by what the user entered
+
+
+##################################################################
+# 2. Do some work with thoese variables
+##################################################################
+
+
+# multiply that number by itself
+result = user_input * user_input
+
+
+##################################################################
+# 3. Output the results
+##################################################################
+
+# print the result
+print(result)
