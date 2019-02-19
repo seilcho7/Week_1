2190b0abc35a11fca4a35112f81f852006f4629d
Seil Cho <seilcho7@hotmail.com>
2019-02-19 18:17:48 -0500

    print n to m numbers

---
+++ n-to-m.py
@@ -0,0 +1,13 @@
+start = int(input("Start from: "))
+end = int(input("End on: "))
+
+
+while start <= end:
+    print(start)
+    start = start + 1
+    if start == end:
+        print(start)
+        break
+
+
+
